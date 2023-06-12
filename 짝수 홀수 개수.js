053cd3f5e2f11fa81b3e74166d40fc2e1ec1f21f
이도현 <84783014+dohyun2lee@users.noreply.github.com>
2023-06-12 18:08:12 +0900

    Create 짝수 홀수 개수.js

---
+++ 짝수 홀수 개수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    var answer = [];
+    var even = 0;
+    var odd = 0;
+    
+    for (var i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 == 0) {
+            even++
+        } else {
+            odd++
+        }
+    }
+    
+    answer.push(even, odd)
+    
+    return answer;
+}
