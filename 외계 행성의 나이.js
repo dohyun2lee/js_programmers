5e4a7c51d2ddcb4eda56c842b5253cfd096102d8
이도현 <84783014+dohyun2lee@users.noreply.github.com>
2023-06-21 16:54:20 +0900

    Create 외계 행성의 나이.js

---
+++ 외계 행성의 나이.js
@@ -0,0 +1,12 @@
+function solution(age) {
+    var answer = '';
+    
+    var stringAge = String(age);
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    
+    for(var i = 0; i < stringAge.length; i++) {
+        answer += alphabet[stringAge[i]]
+    }
+  
+    return answer;
+}
