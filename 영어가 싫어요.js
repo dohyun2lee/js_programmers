779b0b52334d6e8a0a38e6e3f985252db6733ff0
이도현 <84783014+dohyun2lee@users.noreply.github.com>
2023-07-06 01:02:30 +0900

    Create 영어가 싫어요.js

---
+++ 영어가 싫어요.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const numObj = {
+      zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9,
+    }
+    
+    let answer = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return numObj[v];
+    });
+    
+    answer = parseInt(answer);
+    
+    return answer;
+}
